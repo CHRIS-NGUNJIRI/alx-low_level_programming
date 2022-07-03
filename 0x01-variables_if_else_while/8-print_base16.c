136baf21f023825d160afe13f8abf4bc794c1f99
CHRIS-NGUNJIRI <ngothochris73@gmail.com>
2022-07-03 08:37:50 -0700

    okay

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+unsigned char a = '0';
+int 1;
+for (1 = 0; 1 < 10; 1++)
+{
+putchar('0' + a);
+a++;
+}
+putchar('\n');
+return (0);
+}
